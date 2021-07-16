f99ce32951d9ba163aad873e4ddadd782dd5e526
Zach Lambert <zachariah.lambert@dutchie.com>
2021-07-16 18:23:39 -0400

    v.0.0.2

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  target: 'web',
+  mode: process.env.NODE_ENV,
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    clean: true,
+    library: {
+      name: '@artisnull/gin',
+      type: 'umd',
+      umdNamedDefine: true,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        // options: {
+        //   configFile: 'tsconfig.json'
+        // },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+};
